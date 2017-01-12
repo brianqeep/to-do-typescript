dbdbe538a9e0050e0f81841d5fcb8e6230569bf7
brianqeep <brites009@gmail.com>
2017-01-12 12:12:19 +0300

    forms created

---
+++ app/to-do.js
@@ -0,0 +1,14 @@
+var toDoList = (function () {
+    function toDoList(category, list) {
+        this.category = category;
+        this.list = list;
+    }
+    return toDoList;
+}());
+window.onload = function () {
+    var category1 = document.getElementById('listCategory');
+    var list1 = document.getElementById('listItem');
+};
+function listDisplay() {
+    //alert();
+}
---
+++ app/to-do.ts
@@ -0,0 +1,19 @@
+
+class toDoList{
+  category: HTMLElement;
+  list: HTMLElement;
+
+  constructor ( category:HTMLElement, list:HTMLElement){
+    this.category = category;
+    this.list = list;
+  }
+}
+
+window.onload = () => {
+  var category1: HTMLElement = document.getElementById('listCategory');
+  var list1: HTMLElement = document.getElementById('listItem');
+};
+
+function listDisplay(){
+  //alert();
+}
